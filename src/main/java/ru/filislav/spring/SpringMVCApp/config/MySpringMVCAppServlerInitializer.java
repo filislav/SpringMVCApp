2d60cdf66208853c61840fc2725b8fdd839146b8
Вячеслав Филиппов <slava@MacBook-Pro-Vaceslav.local>
2020-09-27 17:21:21 +0400

    lessons

---
+++ src/main/java/ru/filislav/spring/SpringMVCApp/config/MySpringMVCAppServlerInitializer.java
@@ -0,0 +1,20 @@
+package ru.filislav.spring.SpringMVCApp.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCAppServlerInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
